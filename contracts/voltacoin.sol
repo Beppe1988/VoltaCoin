633207b0c6c1042e183b94b4ec990116cc7b8b8b
Giuseppe V <whitewolf_450@hotmail.com>
2020-11-25 17:47:00 +0100

    Edit voltacoin.sol

--- contracts/voltacoin.sol
+++ contracts/voltacoin.sol
@@ -1,3 +1,11 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.7.0;
+
+import "contracts/token/ERC20/ERC20.sol" ;
+
+contract VoltaCoin is SafeERC20 {
+    constructor() SafeERC20("VoltaCoin", "VOLT") {
+        _mint(msg.sender, 100000);
+    }
+}
