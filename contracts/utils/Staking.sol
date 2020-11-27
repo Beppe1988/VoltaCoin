837a576612300937c6467747c192fb94774c876a
Beppe <whitewolf_450@hotmail.com>
2020-11-27 22:03:39 +0100

    Added isStakeholder func to Stake Lib

--- contracts/utils/Staking.sol
+++ contracts/utils/Staking.sol
@@ -4,8 +4,18 @@ pragma solidity ^0.7.0;
 
 library Stake {
 
-    function isStakeholder(address addr) public returns(bool){
+    struct Stakeholders{
+        address _address;
+        uint256 _stake;
+    }
 
+    function isStakeholder(address addr, Stakeholders[] storage addrList) public view returns(bool){
+        for(uint256 s = 0; s < addrList.length; s+=1){
+            if(addr == addrList[s]._address){
+                return true;
+            }
+        }
+        return false;
     }
 
 
