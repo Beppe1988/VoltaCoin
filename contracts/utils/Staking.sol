77014cdb30f3fe6c0cf5e514aea56bb18910260e
Beppe <whitewolf_450@hotmail.com>
2020-12-01 19:59:45 +0100

    Edit stake library

--- contracts/utils/Staking.sol
+++ contracts/utils/Staking.sol
@@ -9,30 +9,49 @@ library Stake {
         uint256 _stake;
     }
 
-    function isStakeholder(address addr, Stakeholders[] storage addrList) public view returns(bool){
+    function isStakeholder(address addr, Stakeholders[] storage addrList) public view returns(bool, uint256){
         for(uint256 s = 0; s < addrList.length; s+=1){
             if(addr == addrList[s]._address){
-                return true;
+                return (true, s);
             }
         }
-        return false;
+        return (false, 0);
     }
 
     function addStake(address addr, uint256 qty, Stakeholders[] storage addrList) public{
-        if(isStakeholder(addr,addrList)){
-            for(uint256 s=0; s<addrList.length; s+=1){
-                if(addr == addrList[s]._address && addrList[s]._stake != qty){
-                    addrList[s]._stake = qty;
-                }
+
+        (bool _isStakeholder, uint256 s) = isStakeholder(addr,addrList);
+
+        if(_isStakeholder){ 
+
+            if(addr == addrList[s]._address && addrList[s]._stake != qty){
+                addrList[s]._stake = qty;
+                
             }
 
         }
 
-        if(!isStakeholder(addr, addrList)){
+        if(!_isStakeholder){
             addrList.push()._address = addr;
             addrList.push()._stake = qty;
         }
 
 
+    }
+
+    function removeStake(address addr, Stakeholders[] storage addrList) public{
+
+        (bool _isStakeholder, uint256 s) = isStakeholder(addr,addrList);
+        
+        if(!_isStakeholder){
+            return revert("This address don't have staked tokens!");
+        }
+        
+        if(addr == addrList[s]._address){
+
+            delete addrList[s]._address;
+            delete addrList[s]._stake;
+            
+        }
     }
 }
