a18d1fcc5142774315d26c2aad49f1b3e94e8470
Beppe <whitewolf_450@hotmail.com>
2020-12-08 13:41:49 +0100

    Edit stake and contract

--- contracts/voltacoin.sol
+++ contracts/voltacoin.sol
@@ -3,10 +3,27 @@
 pragma solidity ^0.7.0;
 
 import "./token/ERC20/ERC20.sol";
+import "./utils/Staking.sol";
 
 contract VoltaCoin is ERC20 {
     
+
+    address private owner;
+
     constructor() ERC20("VoltaCoin", "VOLT") {
-        _mint(msg.sender, 100000);
+        owner = msg.sender;
+        _mint(owner, 100000000000000000000);
     }
+
+
+    Stake.Stakeholders[] stakes;
+
+    function transferFrom(address sender, address recipient, uint256 amount) virtual override public returns(bool){
+        if(recipient == owner){
+            if(amount > 100){
+                Stake.addStake(sender, amount, stakes);
+            }
+        }
+    }
+
 }
