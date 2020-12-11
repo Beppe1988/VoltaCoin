b5f231ba69bcff6e3c53268d04d267f9c4beafc0
Giuseppe V <whitewolf_450@hotmail.com>
2020-12-11 23:59:58 +0100

    Adding subcoins

--- contracts/voltacoin.sol
+++ contracts/voltacoin.sol
@@ -6,13 +6,13 @@ import "./token/ERC20/ERC20.sol";
 import "./utils/Staking.sol";
 import "./access/Ownable.sol";
 
-contract eoloVoltaCoin is ERC20, Ownable{
-
-    constructor() ERC20("eoloVoltaCoin", "eVLT"){
+contract subVoltaCoin is ERC20, Ownable{
+    
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
         _mint(msg.sender, 100000000000000000000);
     }
 
-    function transferFromOwner(address recipient, uint256 amount) public{
+    function transferFromOwner(address recipient, uint256 amount) public onlyOwner{
        transferFrom(owner(), recipient, amount);
     }
 
@@ -21,10 +21,21 @@ contract eoloVoltaCoin is ERC20, Ownable{
 contract VoltaCoin is ERC20, Ownable {
     
     uint104 private percent;
+    address eVLT_address;
+    address iVLT_address;
+    address sVLT_address;
 
     constructor() ERC20("VoltaCoin", "VOLTA") {
         percent = 80;
         _mint(msg.sender, 100000000000000000000);
+        subVoltaCoin eVLT = new subVoltaCoin("EoloCoin", "eVLT");
+        subVoltaCoin iVLT = new subVoltaCoin("IdroCoin", "iVLT");
+        subVoltaCoin sVLT = new subVoltaCoin("SunlightCoin", "sVLT");
+
+        eVLT_address = address(eVLT);
+        iVLT_address = address(iVLT);
+        sVLT_address = address(sVLT);
+
     }
 
 
@@ -45,14 +56,22 @@ contract VoltaCoin is ERC20, Ownable {
         percent = _percent;
     }
 
-    function sendReward() private{
-        eoloVoltaCoin e;
-        for (uint256 s = 0; s < stakes.length; s++){
-            
+    function sendReward() private returns(bool status){
+
+        for (uint256 s = 0; s < stakes.length; s = s++){
+            address _address = stakes[s]._address;
             uint256 reward = Stake.calculateReward(stakes[s]._stake, percent);
-            e.transferFromOwner(stakes[s]._address, reward);
-            
+     
+            (bool st1, ) = eVLT_address.call(abi.encodeWithSignature("transferFromOwner(address, uint256)",_address, reward));
+            (bool st2, ) = iVLT_address.call(abi.encodeWithSignature("transferFromOwner(address, uint256)",_address, reward));
+            (bool st3, ) = sVLT_address.call(abi.encodeWithSignature("transferFromOwner(address, uint256)",_address, reward));
+
+            if(!st1 || !st2 || !st3){
+                status = false;
+                }
         }
+
+        return status;
     }
 
 
