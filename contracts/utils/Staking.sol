8dd9dadcc2a57d65be3441597d0072fc37d6bb1a
Giuseppe V <whitewolf_450@hotmail.com>
2020-12-12 18:05:08 +0100

    adding comments

--- contracts/utils/Staking.sol
+++ contracts/utils/Staking.sol
@@ -2,14 +2,30 @@
 
 pragma solidity ^0.7.0;
 
+/**
+ * @dev A stake library that allows ERC20 tokens holders to stake their tokens
+ * simply send them to tokens owner or via Javascript.  
+ *
+ * The reward 
+ */
 library Stake {
 
+    /**
+     * @dev a new struct type that memorize the address of the staker, 
+     * amount of the stake and the date when it was created or when it
+     * receive last reward. 
+     */
     struct Stakeholders{
         address _address;
         uint256 _stake;
         uint _rewardDate;
     }
 
+    /**
+     * @dev this function check if an address is stakeholder.
+     *
+     * It return boolean status and the position of the address in array.
+     */
     function isStakeholder(address addr, Stakeholders[] storage addrList) internal view returns(bool, uint256){
         for(uint256 s = 0; s < addrList.length; s+=1){
             if(addr == addrList[s]._address){
@@ -19,6 +35,9 @@ library Stake {
         return (false, 0);
     }
 
+    /**
+     * @dev this function add a stakeholder or update its amount.
+     */
     function addStake(address addr, uint256 qty, Stakeholders[] storage addrList) internal{
 
         (bool _isStakeholder, uint256 s) = isStakeholder(addr,addrList);
@@ -40,7 +59,13 @@ library Stake {
 
 
     }
-
+    /**
+     * This function remove a stakeholder.
+     *
+     * The _change variable is a setup for web apps in java that can allow 
+     *
+     * the user to remove arbitrary amount of stake.
+     */
     function removeStake(address addr, Stakeholders[] storage addrList, uint256 _change) internal{
 
         (bool _isStakeholder, uint256 s) = isStakeholder(addr,addrList);
@@ -62,7 +87,9 @@ library Stake {
             
         }
     }
-
+    /**
+    * This function calculate reward for the stakeholders by passing a custom percentage. 
+    */
     function calculateReward(uint256 _stake, uint104 percent) internal pure returns(uint256 data){
         data = (_stake * percent) / 100;
         return data;
--- contracts/voltacoin.sol
+++ contracts/voltacoin.sol
@@ -6,6 +6,9 @@ import "./token/ERC20/ERC20.sol";
 import "./utils/Staking.sol";
 import "./access/Ownable.sol";
 
+/**
+ * This contract is ownable and create a custom subcoin.
+ */
 contract subVoltaCoin is ERC20, Ownable{
     
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol){
@@ -18,6 +21,9 @@ contract subVoltaCoin is ERC20, Ownable{
 
 }
 
+/** 
+ * This create a main token contract.
+ */
 contract VoltaCoin is ERC20, Ownable {
     
     uint104 private percent;
@@ -25,6 +31,15 @@ contract VoltaCoin is ERC20, Ownable {
     address iVLT_address;
     address sVLT_address;
 
+    /** 
+     * Constructor contain: 
+     *
+     * percent: reward percentage;
+     *
+     * Subcoins contract creation;
+     *
+     * Subcoins contract addresses;
+     */
     constructor() ERC20("VoltaCoin", "VOLTA") {
         percent = 80;
         _mint(msg.sender, 100000000000000000000);
@@ -38,9 +53,14 @@ contract VoltaCoin is ERC20, Ownable {
 
     }
 
-
+    // Stakeholder variable in library
     Stake.Stakeholders[] stakes;
 
+    /**
+     * This override ERC20 function, adding the staking mechanism that
+     * add or remove staking by sending or receiving main token amount
+     * to the owner. 
+     */
     function transferFrom(address _sender, address _recipient, uint256 _amount) virtual override public returns(bool){
         if(_recipient == owner()){
             if(_amount > 100){
@@ -52,10 +72,14 @@ contract VoltaCoin is ERC20, Ownable {
         }
     }
 
+    // This change reward percentage
     function percentRewardChange(uint104 _percent) private{
         percent = _percent;
     }
 
+    /**
+     * This function send rewards to stakeholders and return a status. 
+     */
     function sendReward() private returns(bool status){
 
         for (uint256 s = 0; s < stakes.length; s = s++){
